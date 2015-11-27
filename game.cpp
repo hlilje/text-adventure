3d08246d3e24d688adbd603caa3a2a9438fe800b
Hampus Liljekvist <hlilje@users.noreply.github.com>
2015-11-27 13:56:23 +0100

    Refactor input loop and add prompt/help text

--- game.cpp
+++ game.cpp
@@ -33,27 +33,40 @@ void print_greeting() {
         << "| a computer.                                          |\n"
         << "| Only one thing is certain; it is time to pick your   |\n"
         << "| class.                                               |\n"
-        << "| Enter 'start x' with x = 1 for Warrior, 2 for Mage,  |\n"
+        << "| Enter 'choose x' with x = 1 for Warrior, 2 for Mage, |\n"
         << "| and 3 for Peasant.                                   |\n"
         << "========================================================\n"
-        << "\n"
-        << "Commands:\n"
-        << "- go: GO.\n"
-        << "- exit: Exit the game.\n"
     << std::endl;
 }
 
-void split(const std::string &s, const char delim,
-           std::vector<std::string> &elems) {
-    std::stringstream ss(s);
-    std::string item;
-    while (std::getline(ss, item, delim))
-        elems.push_back(item);
+/**
+ * Print the help text.
+ */
+void print_help() {
+    std::cout
+    << "Commands:\n"
+    << "- go: GO.\n"
+    << "- exit: Exit the game.\n"
+    << std::endl;
 }
 
+/**
+ * Print the input prompt.
+ */
+void print_prompt() {
+    std::cout << "> ";
+}
+
+/**
+ * Split the given string using the given delimiter.
+ */
 std::vector<std::string> split(const std::string &s, const char delim) {
     std::vector<std::string> elems;
-    split(s, delim, elems);
+    std::stringstream ss(s);
+    std::string item;
+
+    while (std::getline(ss, item, delim))
+        elems.push_back(item);
 
     return elems;
 }
@@ -68,14 +81,45 @@ void act() {
  */
 void run() {
     print_greeting();
+    print_help();
 
+    bool started = false;
+    print_prompt();
     for (std::string cmd; std::getline(std::cin, cmd);) {
         std::vector<std::string> cmds = split(cmd, ' ');
-        if (cmd == "exit") {
-            break;
-        } else if (cmds.size() > 2) {
+
+        if (cmd == "exit") break;
+
+        if (cmd == "help") {
+            print_help();
+            print_prompt();
+            continue;
+        }
+
+        if (cmds.size() > 2) {
             std::cout << "I only understand two words at a time." << std::endl;
-        } else if (cmds.size() > 0 && cmds[0] == "go") {
+            std::cout << std::endl;
+            print_prompt();
+            continue;
+        }
+
+        if (!started) {
+            if (cmds.size() > 0 && cmds[0] == "choose") {
+                if (cmds.size() < 2) {
+                    std::cout << "Who do you want to be?" << std::endl;
+                } else {
+                    std::cout << "CHOOSE" << std::endl;
+                    started = true;
+                }
+            } else {
+                std::cout << "You must choose a class before continuing." << std::endl;
+            }
+            std::cout << std::endl;
+            print_prompt();
+            continue;
+        }
+
+        if (cmds.size() > 0 && cmds[0] == "go") {
             if (cmds.size() < 2) {
                 std::cout << "Go where?" << std::endl;
             } else {
@@ -96,16 +140,22 @@ void run() {
                 std::cout << "DROP" << std::endl;
                 act();
             }
-        } else if (cmd == "near") {
-            std::cout << "NEAR" << std::endl;
-        } else if (cmd == "where") {
-            std::cout << "WHERE" << std::endl;
+        } else if (cmds.size() > 0 && cmds[0] == "attack") {
+            if (cmds.size() < 2) {
+                std::cout << "Attack what?" << std::endl;
+            } else {
+                std::cout << "ATTACK" << std::endl;
+                act();
+            }
+        } else if (cmd == "look") {
+            std::cout << "LOOK" << std::endl;
         } else if (cmd == "") {
             // Ignore
         } else {
             std::cout << "I don't know what '" << cmd << "' means." << std::endl;
         }
         std::cout << std::endl;
+        print_prompt();
     }
 }
 
