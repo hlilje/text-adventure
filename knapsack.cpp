a619b2ec3f5b7a64b5dbe58126ed40852acc7e26
Hampus Liljekvist <hlilje@users.noreply.github.com>
2015-12-09 17:10:38 +0100

    Add consume test

--- knapsack.cpp
+++ knapsack.cpp
@@ -4,4 +4,4 @@ using namespace text_adventure;
 
 
 Knapsack::Knapsack(const int max_volume, const int max_weight) :
-    Container("knapsack", 10, 2, false, max_volume, max_weight, false) {}
+    Container("knapsack", 10, 10, false, max_volume, max_weight, false) {}
